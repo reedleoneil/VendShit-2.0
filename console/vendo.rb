00216f83fd2b9a77da63625b9165bafe860861d8
reedleoneil <reedleoneilpascual@yahoo.com>
2019-07-28 00:26:22 +0900

    Added mqtt client to backend bridge and added websocket config.

--- console/vendo.rb
+++ console/vendo.rb
@@ -1,7 +1,17 @@
 require 'rubyserial'
 require 'json'
+require 'paho-mqtt'
 
 serialport = Serial.new '/dev/ttyUSB0'
+client = PahoMqtt::Client.new()
+
+client.on_message do |p|
+	puts "Topic: #{p.topic}\nPayload: #{p.payload}\nQoS: #{p.qos}"
+	serialport.write(p.payload)
+end
+
+client.connect('localhost', 1883, client.keep_alive, true, true)
+client.subscribe(["vendo/servo", 2])
 
 Thread.new {
 	loop do
@@ -16,9 +26,12 @@ Thread.new {
 			if x != '' then payload << x end
 		end
 		puts payload
+		payload = JSON.parse(payload)
+		client.publish("vendo/credit", payload['credit'], false, 1)
 	end
 }
 
 loop do
-
+	client.loop_write
+	client.loop_read
 end
